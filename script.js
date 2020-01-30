711487b86597be04e1b12b21b7e19ebb577b3a06
Ilya Smirnov <smilya13@gmail.com>
2020-01-30 21:21:28 +0300

    feat: one more addition to previous commit

--- script.js
+++ script.js
@@ -146,8 +146,30 @@ document.getElementById("calculate").onclick = function() {
   initialDataObj = makeDataObj(initialSalaries, initialDeposits, initialLiabilities);
   yearsArr = calculateYears(initialDataObj);
   chartData = chartDataMaker(yearsArr);
-  drawBasic(chartData);
-  
-  
+  drawBasic(chartData);  
 }
 
+//===========================
+google.charts.load('current', {packages: ['corechart', 'line']});
+//google.charts.setOnLoadCallback(drawBasic); // это запускает отрисовку при загрузке
+
+function drawBasic() {
+
+  var data = new google.visualization.DataTable();
+  data.addColumn('number', 'X');
+  data.addColumn('number', 'Amount');
+  // Чтобы добавить еще один график, добавляем колонки
+  // data.addColumn('number', 'New');
+  // И в каждом подмассиве data добавляем третий показатель - координаты нового графика
+  
+  data.addRows(chartData);
+
+  var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+  chart.draw(data, options);
+
+}
+
+
+
+      
